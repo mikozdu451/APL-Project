b57b67a8736ed1ef5a53cbff15ded052a4dab06f
mikozdu451 <56159221+mikozdu451@users.noreply.github.com>
2021-12-03 16:50:37 +0100

    Add files via upload
    C++ solution

---
+++ CPP-Greyscale/cmake-build-cmakelists/CMakeFiles/CPP_Greyscale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPP_Greyscale.dir/main.cpp.obj"
+  "CPP_Greyscale.exe"
+  "CPP_Greyscale.exe.manifest"
+  "CPP_Greyscale.pdb"
+  "libCPP_Greyscale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPP_Greyscale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CPP-Greyscale/cmake-build-cmakelists/CMakeFiles/CPP_Greyscale.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPP_Greyscale.
+# This may be replaced when dependencies are built.
